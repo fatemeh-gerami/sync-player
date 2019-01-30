8a9323005df0a3e10f53c1544f9610e94afef6bc
fatemeh-gerami <fatemehgerami32@gmail.com>
2019-01-30 18:35:28 +0330

    first commit

--- syncPlayer/ViewController.swift
+++ syncPlayer/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController<MRBServerDelegate> {
+
+    @IBOutlet weak var Send: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func sendAction(_ sender: Any) {
+        print("hi hi hi")
+        
+    }
 }
 
